3f40d4c6db9d88635b446daf0de858557565fb90
Morgscode <morgan.luke94@gmail.com>
2024-10-19 19:27:07 +0100

    tidy up

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Renderer from "./renderer";
-import * as shaders from './shaders/index';
+import * as shaders from './shaders';
 import './style.css';
 
-const shader = shaders["textures"];
+const shader = shaders["first"];
 const shaderRenderer = new Renderer(shader);
 shaderRenderer.init();
