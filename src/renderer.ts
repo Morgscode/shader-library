28860415ee775b3f795d503d96d7f8254fbb319a
Morgscode <morgan.luke94@gmail.com>
2024-10-19 17:50:03 +0100

    refactor renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,61 +3,61 @@ import type { Shader } from "./shaders";
 
 export default class Renderer {
 
-    _threejs: THREE.WebGLRenderer;
-    _scene: THREE.Scene;
-    _camera: THREE.Camera;
-    _textureLoader: THREE.TextureLoader;
-    _vsh: string;
-    _fsh: string;
-    _app: HTMLDivElement | null;
+    threejs: THREE.WebGLRenderer;
+    scene: THREE.Scene;
+    camera: THREE.Camera;
+    textureLoader: THREE.TextureLoader;
+    vsh: string;
+    fsh: string;
+    app: HTMLDivElement | null;
 
     constructor(shader: Shader) {
-        this._app = document.querySelector("#app");
-        this._threejs = new THREE.WebGLRenderer();
-        this._scene = new THREE.Scene();
-        this._camera = new THREE.OrthographicCamera(0, 1, 1, 0, 0.1, 2000);
-        this._textureLoader = new THREE.TextureLoader()
-        this._vsh = shader.vertex;
-        this._fsh = shader.fragment;
+        this.app = document.querySelector("#app");
+        this.threejs = new THREE.WebGLRenderer();
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.OrthographicCamera(0, 1, 1, 0, 0.1, 2000);
+        this.textureLoader = new THREE.TextureLoader()
+        this.vsh = shader.vertex;
+        this.fsh = shader.fragment;
     }
 
-    async init() {
-       if (!this._app) return;
-       this._app.appendChild(this._threejs.domElement);
-       window.addEventListener('resize', (e) => this._onWindowResize(e));
-       this._camera.position.set(0, 0, 1);
-       await this.shader()
-       this._raf();
+    init() {
+       if (!this.app) return;
+       this.app.appendChild(this.threejs.domElement);
+       window.addEventListener('resize', (e) => this.onWindowResize(e));
+       this.camera.position.set(0, 0, 1);
+       this.shader()
+       this.animate();
     }
 
-    async shader() {
-        const texture = this._textureLoader.load('./assets/images/trippy-ground.png');
+    shader() {
+        const texture = this.textureLoader.load('./assets/images/trippy-ground.png');
 
         // https://threejs.org/docs/#api/en/materials/ShaderMaterial
         const material = new THREE.ShaderMaterial({
             uniforms: {
                 diffuse: {value: texture}
             },
-            vertexShader: this._vsh,
-            fragmentShader: this._fsh
+            vertexShader: this.vsh,
+            fragmentShader: this.fsh
         });
 
         // https://threejs.org/docs/#api/en/geometries/PlaneGeometry
         const geometry = new THREE.PlaneGeometry(1, 1);
         const plane = new THREE.Mesh(geometry, material);
         plane.position.set(0.5, 0.5, 0);
-        this._scene.add(plane);
-        this._onWindowResize(null);
+        this.scene.add(plane);
+        this.onWindowResize(null);
     }
 
-    _onWindowResize(_: UIEvent | null) {
-        this._threejs.setSize(window.innerWidth, window.innerHeight);
+    onWindowResize(_: UIEvent | null) {
+        this.threejs.setSize(window.innerWidth, window.innerHeight);
     }
 
-    _raf() {
-        requestAnimationFrame((_) => {
-            this._threejs.render(this._scene, this._camera);
-            this._raf();
+    animate() {
+        requestAnimationFrame(() => {
+            this.threejs.render(this.scene, this.camera);
+            this.animate();
         })
     }
 
