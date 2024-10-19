956e2e5e0522008fc8803f2f5204f4dec80791ae
Morgscode <morgan.luke94@gmail.com>
2024-10-19 21:11:56 +0100

    shader update

--- src/shaders/first/fragment.glsl
+++ src/shaders/first/fragment.glsl
@@ -3,5 +3,5 @@ uniform float u_time;
 varying vec2 v_uv;
 
 void main() {
-    gl_FragColor = vec4(v_uv.y, abs(u_time), v_uv.x, 1.0);
+    gl_FragColor = vec4(v_uv.y, cos(sqrt(u_time)), v_uv.x, 1.0);
 }
