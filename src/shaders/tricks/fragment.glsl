6207e0f6b1e9bff30457a761753056c7bb72dd2c
Morgscode <morgan.luke94@gmail.com>
2024-10-25 17:38:57 +0100

    refactor examples

--- src/shaders/tricks/fragment.glsl
+++ src/shaders/tricks/fragment.glsl
@@ -23,24 +23,24 @@ varying vec2 v_uv;
 // }
 
 // draw a straight vertical line
-// void main() {
-//     vec3 color = vec3(0.0);
-//     float line = smoothstep(0.0001, 0.005, abs(v_uv.x - 0.5));
-//     color = vec3(line);
-
-//     color = mix(vec3(0.0), color, vec3(line));
-//     gl_FragColor = vec4(color, 1.0);
-// }
-
-// void animation
 void main() {
     vec3 color = vec3(0.0);
-    float line = smoothstep(0.0, 1.0, abs(v_uv.x - 0.5));
-    color = abs(sin(vec3(line) * u_time * 130.0));
+    float line = smoothstep(0.0001, 0.005, abs(v_uv.x - 0.5));
+    color = vec3(line);
 
-    color = mix(vec3(0.0), color, vec3(line));
+    color = mix(vec3(0.0), color, line);
     gl_FragColor = vec4(color, 1.0);
 }
+
+// void animation
+// void main() {
+//     vec3 color = vec3(0.0);
+//     float line = smoothstep(0.0, 1.0, abs(v_uv.x - 0.5));
+//     color = abs(sin(vec3(line) * u_time * 130.0));
+
+//     color = mix(vec3(0.0), color, line);
+//     gl_FragColor = vec4(color, 1.0);
+// }
 
 // drawing lines
 // void main() {
