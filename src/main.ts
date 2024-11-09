e1c4f61169c99e283c8bf4e50c4bbbc8e9a33ced
Morgscode <morgan.luke94@gmail.com>
2024-11-09 16:39:39 +0000

    refactor first shaders

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Renderer from "./renderer";
 import * as shaders from './shaders';
 import './style.css';
 
-const shader = shaders["textures"];
-const shaderRenderer = new Renderer(shader);
-shaderRenderer.init();
+const shader = shaders["first"];
+const renderer = new Renderer(shader);
+renderer.init();
 
