3ed061d3658b17630794c26d395f8cc1cb88e300
Morgscode <morgan.luke94@gmail.com>
2024-11-10 11:37:24 +0000

    move around drawing shaders

--- src/shaders/drawing/fragment.glsl
+++ src/shaders/drawing/fragment.glsl
@@ -10,15 +10,84 @@ float plot(vec2 st, float pct) {
           smoothstep( pct, pct + 0.02, st.y);
 }
 
+// draw a striaght line
 void main() {
-    vec2 st = gl_FragCoord.xy/u_resolution;
+    vec3 color = vec3(v_uv.x);
 
-    float y = pow(st.x, PI);
-
-    vec3 color = vec3(y);
-
-    float pct = plot(st, y);
-    color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
+    float line = smoothstep(0.02, 0.0, abs(v_uv.x - v_uv.y));
+    color = (1.0 - line) * color + line * vec3(0.0, 1.0, 0.0);
 
     gl_FragColor = vec4(color, 1.0);
 }
+ 
+// draw a curved line
+// void main() {
+//     vec2 uv = v_uv * 2.0 - 1.0;
+//     float y = pow(v_uv.x, PI);
+
+//     vec3 color = vec3(y);
+
+//     float pct = plot(v_uv, y);
+//     color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
+
+//     gl_FragColor = vec4(color, 1.0);
+// }
+
+// draw a responsive circle with SDF
+// void main() {
+//     vec2 uv = v_uv * 2.0 - 1.0;
+//     uv.x *= u_resolution.x / u_resolution.y;
+
+//     float l = length(uv) - 0.5;
+
+//     gl_FragColor = vec4(l, l, l, 1.0);
+// }
+
+// hypnotic
+// void main() {
+//     vec2 uv = v_uv * 2.0 - 1.0;
+//     uv.x *= u_resolution.x / u_resolution.y;
+
+//     float l = length(uv);
+
+//     l = sin(l * 8.0 + u_time) / 8.0;
+//     l = abs(l);
+
+//     l = smoothstep(0.0, 0.1, l);
+
+//     gl_FragColor = vec4(l, l, l, 1.0);
+// }
+
+// cosine based palette
+vec3 palette(float t) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.0, 0.3, 0.6);
+
+    return a + b * cos(6.283185 * (c * t + d));
+}
+
+// procedural geomtery
+// void main() {
+//     vec2 uv = v_uv * 2.0 - 1.0;
+//     uv.x *= u_resolution.x / u_resolution.y;
+//     vec2 l_uv = uv;
+//     vec3 final = vec3(0.0);
+
+//     for (float i = 0.0; i < 3.0; i++) {
+//         uv = fract(uv * PI) - 0.5;
+
+//         float l = length(uv) * exp(-length(l_uv));
+//         vec3 color = palette(length(l_uv) + i * 0.4 + u_time * 1.30);
+
+//         l = sin(l * 8.0 + u_time) / 8.0;
+//         l = abs(l);
+
+//         l = pow(0.01 / l, 1.5);
+
+//         final += color * l;
+//     }
+
+//     gl_FragColor = vec4(final, 1.0);
+// }
