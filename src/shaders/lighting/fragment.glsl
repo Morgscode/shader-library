8c0fdb774b6816d9f7bf1fb13239fa00ce07d25a
Morgscode <morgan.luke94@gmail.com>
2024-12-06 14:23:27 +0000

    add lighting examples

--- src/shaders/lighting/fragment.glsl
+++ src/shaders/lighting/fragment.glsl
@@ -1,18 +1,80 @@
 varying vec2 v_uv;
+varying vec3 v_normal;
 
-// ambient lighting
-void main() {
-    vec3 base = vec3(0.5);
-    vec3 lighting = vec3(0.0);
-    vec3 ambient = vec3(0.5);
-    lighting = ambient;
-    vec3 color = base * lighting;
-    gl_FragColor = vec4(color, 1.0);
+float inverseLerp(float v, float minVal, float maxVal) {
+    return (v - minVal) / (maxVal - minVal);
 }
 
-// hemisphere lighting
+float remap(float v, float inMin, float inMax, float outMin, float outMax) {
+    float t = inverseLerp(v, inMin, inMax);
+    return mix(outMin, outMax, t);
+}
+
+vec3 linearTosRGB(vec3 value ) {
+  vec3 lt = vec3(lessThanEqual(value.rgb, vec3(0.0031308)));
+  
+  vec3 v1 = value * 12.92;
+  vec3 v2 = pow(value.xyz, vec3(0.41666)) * 1.055 - vec3(0.055);
+
+	return mix(v2, v1, lt);
+}
+
+// ambient lighting
 // void main() {
-//     vec3 color = vec3(0.0);
+//     vec3 base = vec3(0.5);
+//     vec3 lighting = vec3(0.0);
+//     vec3 ambient = vec3(0.5);
+//     lighting = ambient;
+//     vec3 color = base * lighting;
 //     gl_FragColor = vec4(color, 1.0);
 // }
 
+// hemisphere lighting
+// void main() {
+//     vec3 base = vec3(0.5);
+//     vec3 lighting = vec3(0.0);
+//     vec3 normal = normalize(v_normal);
+
+//     vec3 ambient = vec3(0.5);
+
+//     vec3 sColor = vec3(0.0, 0.3, 0.6);
+//     vec3 gColor = vec3(0.6, 0.3, 0.1);
+
+//     float hemiMix = remap(normal.y, -1.0, 1.0, 0.0, 1.0);
+//     vec3 hemi = mix(gColor, sColor, hemiMix);
+
+//     lighting = ambient + 0.0 + hemi;
+
+//     vec3 color = base * lighting;
+
+//     gl_FragColor = vec4(color, 1.0);
+// }
+
+// lambertian light source 
+void main() {
+    vec3 base = vec3(0.5);
+    vec3 lighting = vec3(0.0);
+    vec3 normal = normalize(v_normal);
+
+    vec3 ambient = vec3(0.5);
+
+    vec3 sColor = vec3(0.0, 0.3, 0.6);
+    vec3 gColor = vec3(0.6, 0.3, 0.1);
+
+    float hemiMix = remap(normal.y, -1.0, 1.0, 0.0, 1.0);
+    vec3 hemi = mix(gColor, sColor, hemiMix);
+
+    vec3 lightDirection = normalize(vec3(1.0, 1.0, 1.0));
+    vec3 lightColor = vec3(1.0, 1.0, 0.9);
+    float dotProduct = max(0.0, dot(lightDirection, normal));
+
+    vec3 diffuse = dotProduct * lightColor;
+
+    lighting = ambient + 0.0 + hemi * 0.5 + diffuse * 0.5;
+
+    vec3 color = base * lighting;
+
+    color = linearTosRGB(color);
+
+    gl_FragColor = vec4(color, 1.0);
+}
--- src/shaders/lighting/vertex.glsl
+++ src/shaders/lighting/vertex.glsl
@@ -1,7 +1,9 @@
 varying vec2 v_uv;
+varying vec3 v_normal;
 
 void main () {
     vec4 localPosition = vec4(position, 1.0);
     gl_Position = projectionMatrix * modelViewMatrix * localPosition;
     v_uv = uv;
+    v_normal = (modelMatrix * vec4(normal, 0.0)).xyz;
 }
