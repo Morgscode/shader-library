09a6c82b5c0ec0fa853344646e744834acd06363
Morgscode <morgan.luke94@gmail.com>
2024-12-09 17:55:14 +0000

    vertex shader examples@

--- src/shaders/vertex/fragment.glsl
+++ src/shaders/vertex/fragment.glsl
@@ -2,6 +2,7 @@ uniform samplerCube u_specmap;
 
 varying vec3 v_normal;
 varying vec3 v_position;
+varying vec4 v_color;
 
 float inverseLerp(float v, float minVal, float maxVal) {
     return (v - minVal) / (maxVal - minVal);
@@ -19,9 +20,32 @@ vec3 linearTosRGB(vec3 value ) {
     return mix(v2, v1, lt);
 }
 
-// specular IBL with fresnel
+
 void main() {
-    vec3 base = vec3(0.5);
+    vec3 base = v_color.xyz;
+
+    vec3 red = vec3(1.0, 0.0, 0.0);
+    vec3 blue = vec3(0.0, 0.0, 1.0);
+    vec3 yellow = vec3(1.0, 1.0, 0.0);
+
+    float value_1 = v_color.w;
+    float line_one = smoothstep(0.003, 0.004, abs(v_position.y - mix(-0.5, 0.0, value_1)));
+
+    base = mix(yellow, base, line_one);
+
+    if (v_position.y > 0.0) {
+        float t = remap(v_position.x, -0.5, 0.5, 0.0, 1.0);
+        t = pow(t, 2.0);
+        base = mix(red, blue, t);
+
+        float value_2 = t;
+        float line_two = smoothstep(0.003, 0.004, abs(v_position.y - mix(0.0, 0.5, value_2)));
+        base = mix(yellow, base, line_two);
+    }
+
+    float middleLine = smoothstep(0.004, 0.005, abs(v_position.y));
+    base = mix(vec3(0.0), base, middleLine);
+
     vec3 lighting = vec3(0.0);
     vec3 normal = normalize(v_normal);
     vec3 viewDirection = normalize(cameraPosition - v_position);
@@ -59,7 +83,7 @@ void main() {
 
     vec3 color = base * lighting + specular;
 
-    //color = linearTosRGB(color);
+    color = linearTosRGB(color);
 
     gl_FragColor = vec4(color, 1.0);
 }
--- src/shaders/vertex/vertex.glsl
+++ src/shaders/vertex/vertex.glsl
@@ -1,10 +1,73 @@
 varying vec3 v_normal; 
 varying vec3 v_position;
+varying vec4 v_color;
 
 uniform float u_time;
 
+float inverseLerp(float v, float minVal, float maxVal) {
+    return (v - minVal) / (maxVal - minVal);
+}
+
+float remap(float v, float inMin, float inMax, float outMin, float outMax) {
+    float t = inverseLerp(v, inMin, inMax);
+    return mix(outMin, outMax, t);
+}
+
+mat3 rotateY(float rads) {
+    float s = sin(rads);
+    float c = cos(rads);
+
+    return mat3(
+        c, 0.0, s,
+        0.0, 1.0, 0.0,
+        -s, 0.0, c
+    );
+}
+
+// simple y-axis transform
+// void main() {
+//     vec3 localSpacePosition = position;
+
+//     localSpacePosition.y += sin(u_time);
+
+//     gl_Position = projectionMatrix * modelViewMatrix * vec4(localSpacePosition, 1.0);
+//     v_normal = (modelMatrix * vec4(normal, 0.0)).xyz;
+//     v_position = (modelMatrix * vec4(position, 1.0)).xyz;
+// }
+
+// 3-d scaling
+// void main() {
+//     vec3 localSpacePosition = position;
+
+//     localSpacePosition *= remap(sin(u_time), -1.0, 1.0, 0.5, 1.5);
+
+//     gl_Position = projectionMatrix * modelViewMatrix * vec4(localSpacePosition, 1.0);
+//     v_normal = (modelMatrix * vec4(normal, 0.0)).xyz;
+//     v_position = (modelMatrix * vec4(position, 1.0)).xyz;
+// }
+
+// simple y-axis rotation
+// void main() {
+//     vec3 localSpacePosition = position;
+
+//     localSpacePosition = rotateY(u_time) * localSpacePosition;
+
+//     gl_Position = projectionMatrix * modelViewMatrix * vec4(localSpacePosition, 1.0);
+//     v_normal = (modelMatrix * vec4(normal, 0.0)).xyz;
+//     v_position = (modelMatrix * vec4(position, 1.0)).xyz;
+// }
+
+// 
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec3 localSpacePosition = position;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(localSpacePosition, 1.0);
+    
     v_normal = (modelMatrix * vec4(normal, 0.0)).xyz;
     v_position = (modelMatrix * vec4(position, 1.0)).xyz;
+
+    vec3 red = vec3(1.0, 0.0, 0.0);
+    vec3 blue = vec3(0.0, 0.0, 1.0);
+
+    float t = remap(v_position.x, -0.5, 0.5, 0.0, 1.0);
+    v_color = vec4(mix(red, blue, t), t);
 }
