44e45411b0a6389ad1eaac29be4df4d2aaecba1d
Morgscode <morgan.luke94@gmail.com>
2024-12-11 19:36:30 +0000

    refactor fractal

--- src/shaders/fractals/fragment.glsl
+++ src/shaders/fractals/fragment.glsl
@@ -1,12 +1,24 @@
+#define PI 3.1415926535
+
 varying vec2 v_uv;
 
 uniform float u_time;
 uniform vec2 u_resolution;
 
+vec3 palette(float t) {
+        vec3 a = vec3(0.5, 0.5, 0.5);
+        vec3 b = vec3(0.75, 0.75, 0.75);
+        vec3 c = vec3(1.0, 1.0, 1.0);
+        vec3 d = vec3(0.0, 0.333, 0.667);
+
+        return a + b * cos((PI * 2.0) * (c * t + d));
+    }
+
 // base fractal
 void main() {
+    // control the speed of the animation
+    float angle = u_time / 13.0;
     //  center our uvs
-    float angle = u_time * 0.3;
     vec2 uv = v_uv * 2.0 - 1.0;
     // make it responsive to the current screen size
     uv.x *= u_resolution.x / u_resolution.y;
@@ -20,6 +32,9 @@ void main() {
             sin(angle), cos(angle)
         );    
     }
+
+    vec3 color = palette(length(uv) + u_time);
+    
     // render the final color
-    gl_FragColor = vec4(vec3(length(uv)), 1.0);
+    gl_FragColor = vec4(vec3(length(uv)) * color, 1.0);
 }
