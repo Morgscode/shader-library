46c84d63a8abac20400e68daf084695951e24fc0
Morgscode <morgan.luke94@gmail.com>
2024-12-20 17:19:33 +0000

    add fbm function

--- src/shaders/noise/fragment.glsl
+++ src/shaders/noise/fragment.glsl
@@ -69,6 +69,25 @@ float noise( in vec3 p )
                         dot( hash( i + vec3(1.0,1.0,1.0) ), f - vec3(1.0,1.0,1.0) ), u.x), u.y), u.z );
 }
 
+float fbm(vec3 p, int octaves, float persistence, float lacunarity) {
+  float amplitude = 0.5;
+  float frequency = 1.0;
+  float total = 0.0;
+  float normalization = 0.0;
+
+  for (int i = 0; i < octaves; ++i) {
+    float noiseValue = noise(p * frequency);
+    total += noiseValue * amplitude;
+    normalization += amplitude;
+    amplitude *= persistence;
+    frequency *= lacunarity;
+  }
+
+  total /= normalization;
+
+  return total;
+}
+
 // simple gradient noise
 // void main() {
 //     vec2 uv = v_uv * 2.0 - 1.0;
