5f8ca4e7d4b5196a73464ff0c9d8cc3c31d856fa
Morgscode <morgan.luke94@gmail.com>
2024-12-21 13:47:40 +0000

    add postfx examples

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ window.addEventListener('load', () => {
     }
 });
 
-const shader = shaders["postfx"];
-const renderer = new Renderer(shader);
-renderer.render();
+// const shader = shaders["postfx"];
+// const renderer = new Renderer(shader);
+// renderer.render();
