e10d076967fce3a3144d3ed9569cc9c84cd257d5
Morgscode <morgan.luke94@gmail.com>
2024-12-26 23:26:14 +0000

    tidy up

--- src/shaders/library/fbm-fractals/fragment.glsl
+++ src/shaders/library/fbm-fractals/fragment.glsl
@@ -17,14 +17,9 @@ vec3 palette(float t)
         return a + b * cos((PI * 2.0) * (c * t + d));
 }
 
-/// The MIT License
-/// Copyright © 2013 Inigo Quilez
-/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-/// https://www.youtube.com/c/InigoQuilez
 /// https://iquilezles.org/
-//
 /// https://www.shadertoy.com/view/Xsl3Dl
-vec3 hash( vec3 p ) /// replace this by something better
+vec3 hash( vec3 p )
 {
 	p = vec3( 
         dot(p, vec3(127.1,311.7, 74.7)),
