50487c6c454d143a6e21652b52473d0cbbb68312
Morgscode <morgan.luke94@gmail.com>
2024-12-29 00:54:36 +0000

    refactor tt2

--- src/shaders/library/texture-techniques-2/fragment.glsl
+++ src/shaders/library/texture-techniques-2/fragment.glsl
@@ -10,10 +10,10 @@ uniform vec4 u_tint;
 
 void main() 
 {   
-    vec3 color = vec3(0.0);
+    vec4 sample1 = texture2D(u_texturemap, v_uv);
     vec2 uv = v_uv;
-    uv.y += (u_time * 0.0005);
-    float t = smoothstep(-abs(sin(u_resolution.y)), abs(sin(u_resolution.y)), sin((-uv.y) * u_resolution.y * 2.0));
-    color = vec3(t);
-    gl_FragColor = texture2D(u_texturemap, v_uv) * vec4(color, 0.1);
+    uv.y += u_time * 0.0005;
+    float line = smoothstep(-0.5, 0.5, sin((-uv.y) * 1000.0));
+    vec3 color = vec3(line);
+    gl_FragColor = sample1 * vec4(color, 0.5);
 }
