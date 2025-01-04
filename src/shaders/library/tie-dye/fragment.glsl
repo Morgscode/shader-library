14bb83dc5797cbd569676c56471c543a10ebcaf6
Morgscode <morgan.luke94@gmail.com>
2025-01-04 00:49:45 +0000

    improve  tie-dye

--- src/shaders/library/tie-dye/fragment.glsl
+++ src/shaders/library/tie-dye/fragment.glsl
@@ -8,7 +8,8 @@ uniform vec2 u_resolution;
 uniform vec2 u_mousepos;
 
 /// https://github.com/Erkaman/glsl-cos-palette
-vec3 palette(float t) {
+vec3 palette(float t) 
+{
     vec3 a = vec3(0.6, 0.5, 0.5);
     vec3 b = vec3(0.5, 0.6, 0.5);
     vec3 c = vec3(0.6, 0.6, 0.5);
@@ -19,14 +20,16 @@ vec3 palette(float t) {
 
 /// https://iquilezles.org/
 /// https://www.shadertoy.com/view/Xsl3Dl
-vec3 hash(vec3 p) {
+vec3 hash(vec3 p) 
+{
     p = vec3(dot(p, vec3(127.1, 311.7, 74.7)), dot(p, vec3(269.5, 183.3, 246.1)), dot(p, vec3(113.5, 271.9, 124.6)));
 
     return -1.0 + 2.0 * fract(sin(p) * 43758.5453123);
 }
 
 /// https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
-float noise(in vec3 p) {
+float noise(in vec3 p) 
+{
     vec3 i = floor(p);
     vec3 f = fract(p);
     vec3 u = f * f * (3.0 - 2.0 * f);
@@ -60,16 +63,13 @@ float turbulence_fbm(
     return total;
 }
 
-/// https://en.wikipedia.org/wiki/Rotation_matrix
-mat2 rotate2d(float angle) {
-    return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
-}
-
-float i_lerp(float value, float min_val, float max_val) {
+float i_lerp(float value, float min_val, float max_val) 
+{
     return (value - min_val) / (max_val - min_val);
 }
 
-float remap(float value, float in_min, float in_max, float out_min, float out_max) {
+float remap(float value, float in_min, float in_max, float out_min, float out_max) 
+{
     float t = i_lerp(value, in_min, in_max);
     return mix(out_min, out_max, t);
 }
@@ -79,13 +79,12 @@ void main() {
     vec2 uv = v_uv * 2.0 - 1.0;
     uv.x *= u_resolution.x / u_resolution.y;
     vec2 l_uv = uv;
-    float angle = u_time * (BPM * 0.001);
+    float angle = u_time * (BPM * 0.0025);
     float noise_sample = turbulence_fbm(vec3(pixel_coords, angle) * 0.005, 2, 0.5, remap(angle, 0.0, angle, -4.0, 4.0));
  
     uv = abs(uv);
     uv -= 0.5;
     uv *= 1.1;
-    uv *= rotate2d(angle);
     uv *= noise_sample;
 
     float l = length(uv) + exp(-length(l_uv));
