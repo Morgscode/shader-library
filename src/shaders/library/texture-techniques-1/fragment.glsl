3a39184dfa45841ecc5ea5faa2171d33cc8faf5f
Morgscode <morgan.luke94@gmail.com>
2025-01-10 16:12:42 +0000

    tidy up tt1

--- src/shaders/library/texture-techniques-1/fragment.glsl
+++ src/shaders/library/texture-techniques-1/fragment.glsl
@@ -7,6 +7,6 @@ uniform sampler2D u_texturemap;
 
 void main() 
 {
-    vec2 uv = mod((v_uv - 0.5) * sin(u_time / 5.0) + 0.5, 1.0);
-    gl_FragColor = texture2D(u_texturemap, uv) * vec4(v_uv.x, abs(sin(u_time)), v_uv.y, 1.0);
+    vec2 uv = mod((v_uv - 0.5) * sin(u_time / 2.6) + 0.5, 1.0);
+    gl_FragColor = texture2D(u_texturemap, uv) * vec4(v_uv.x, abs(sin(u_time * 0.26)), v_uv.y, 1.0);
 }
