4531d4e2be52c5028a3fd09a7a7aadc971c1bb48
Morgscode <morgan.luke94@gmail.com>
2025-02-21 19:15:33 +0000

    refactor cubic

--- src/shaders/library/cubic-julia-set/fragment.glsl
+++ src/shaders/library/cubic-julia-set/fragment.glsl
@@ -43,32 +43,32 @@ void main()
     uv.x *= u_resolution.x / u_resolution.y;
     float angle = u_time * (BPM * 0.001);
     uv.x -= remap(sin(angle), -1.0, 1.0, 0.0, 2.0);
-    uv.y -= remap(sin(angle), -1.0, 1.0, 0.0, 0.5);
+    uv.y -= remap(sin(angle), -1.0, 1.0, -0.5, 0.5);
     uv *= rotate2d(angle / 2.0);
-    vec2 z = uv / remap(-sin(angle), -1.0, 1.0, 5.0, 1.0);
-    vec2 c = vec2(
+    vec2 z = uv / remap(-sin(angle), -1.0, 1.0, 5.0, 2.0);
+  
+    /// https://en.wikipedia.org/wiki/Julia_set
+    /// https://www.shadertoy.com/view/NdSGRG
+      vec2 c = vec2(
         0.5 * remap(cos(2.0 * angle), -1.0, 1.0, 0.95, 1.0), 
         0.5 * remap(sin(3.0 * angle), -1.0, 1.0, 0.95, 1.0)
     );
-    float r = 4.0;
     float iterations = 0.0;
-
-    /// https://en.wikipedia.org/wiki/Julia_set
-    /// https://www.shadertoy.com/view/NdSGRG
     for (float i = 0.0; i < BPM; i++) 
     {
-        if (dot(z, z) > r) break;
+        if (dot(z, z) > 4.0) break;
         float x2 = z.x * z.x - z.y * z.y;
         float y2 = 2.0 * z.x * z.y;
         z = vec2(
             x2 * z.x - y2 * z.y,
             x2 * z.y + y2 * z.x 
         ) + c;
+        iterations += 1.0;
     }
 
     float t = iterations / BPM;
     float l = length(uv) * exp(-length(z));
     vec3 color = palette(l) + t;
 
-    gl_FragColor = vec4(color / (length(uv) + remap(sin(angle), -1.0, 1.0, 1.0, 0.0)), 1.0);
+    gl_FragColor = vec4(color / ((length(uv) + remap(sin(angle), -1.0, 1.0, 3.0, 1.0) / 2.0)), 1.0);
 }
--- src/shaders/library/quadratic-julia-set/fragment.glsl
+++ src/shaders/library/quadratic-julia-set/fragment.glsl
@@ -35,18 +35,17 @@ void main()
     float angle = u_time * (BPM * 0.001);
     uv.x -= remap(sin(angle), -1.0, 1.0, 2.0, 0.0);
     vec2 z = uv / remap(-sin(angle), -1.0, 1.0, 5.0, 3.0);
+
+    /// https://en.wikipedia.org/wiki/Julia_set
+    /// https://www.shadertoy.com/view/NdSGRG
+    float iterations = 0.0;
     vec2 c = vec2(
         -0.8 + 0.2 * remap(cos(angle), -1.0, 1.0, 0.0, 0.25), 
         0.156 + 0.2 * remap(sin(angle), -1.0, 1.0, -0.25, 0.0)
     );
-    float r = 4.0;
-    float iterations = 0.0;
-    
-    /// https://en.wikipedia.org/wiki/Julia_set
-    /// https://www.shadertoy.com/view/NdSGRG
     for (float i = 0.0; i < BPM; i++) 
     {
-        if (dot(z, z) > r) break;
+        if (dot(z, z) > 4.0) break;
         z = vec2(
             z.x * z.x - z.y * z.y, 
             2.0 * z.x * z.y
